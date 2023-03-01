4d6fe9fb98dea5dfd52263201f555f609a650e88
matthew <permahuman@gmail.com>
2023-03-01 00:22:04 -0800

    Add webpack htmlwebpackplugin to automatically inject index.html into bundle dist. Add background image css loader and image laoder. Test loading of random image in unsplash. Finish webpack config for now.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import './style.css';
 
+
+
+
 console.log('hello world')
 
 const content = document.querySelector('#content')
 
 const para = document.createElement('p')
 
-content.appendChild(para)
 
-para.classList.add('para')
 
-para.textContent = 'poooooop'
+
 
 const randomImage1 = document.createElement('img')
-
 randomImage1.classList.add('randomImage1')
+
+content.appendChild(randomImage1)
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode:'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+
+  
+  
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+
   module: {
     rules: [
         {
@@ -16,18 +28,9 @@ module.exports = {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: 'asset/resource',
           },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-        }
-      }
+        
+      
     ]
+    
   }
 };
